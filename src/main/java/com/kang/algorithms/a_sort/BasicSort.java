99636def950f5e2085f9ba6cc19d89de99583473
momo <271858344>
2017-08-21 22:48:42 +0800

    insertion sort

--- src/main/java/com/kang/algorithms/a_sort/BasicSort.java
+++ src/main/java/com/kang/algorithms/a_sort/BasicSort.java
@@ -1,43 +1,61 @@
 package com.kang.algorithms.a_sort;
 /**
- * 功能：选择排序的实现
- * 选择排序的思想：
+ * 功能：基本排序的实现
+ * 1.选择排序的思想：
  * 	对于一个n个元素的数组，通过n趟遍历，在每一趟的遍历中找到最小元素，放到当前最前位置 eg：
  *  在第一次遍历中，初始第一个元素最小，然后进行遍历，找到比第一个元素小的元素的话，交换位置，一趟下来，第一个元素是当前
  *  数组中的最小元素。以此类推
+ * 2.
+ * 
  * @user momo
  * 2017年8月20日
  */
-public class SelectionSort {
+public class BasicSort {
 	
-	public static void sort(int[] arr) {
+	//选择排序
+	public static void selectSort(int[] arr) {
 		int length = arr.length;
 		for(int i = 0; i < length - 1 ; i ++ ) {
 			int min = arr[i];
 			for(int j = i + 1; j < length ; j ++ ) {
 				if(arr[j] < min) {
 					min = arr[j];
-					swap(arr,i,j);
+					SortTestHelper.swap(arr,i,j);
 				}
 			}
 		}
 	}
 	
-	private static void swap(int[] arr,int i,int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
+	
+	//插入排序
+	public static void insertionSort(int[] arr) {
+		int length = arr.length;
+		for(int i = 1; i < length; i ++ ) {
+			for( int j = i; j > 0 && arr[j] < arr[j - 1] ; j-- ) 
+				SortTestHelper.swap(arr,i,j);
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
-		int[] a = SortTestHelper.generateRandomArray(100, 0, 50);
-		sort(a);
-		SortTestHelper.printArray(a);
-		System.out.println();
+		int[] arr = SortTestHelper.generateRandomArray(10, 1, 10);
+		SortTestHelper.testSort(BasicSort.class, "insertionSort", arr);
+		SortTestHelper.printArray(arr);
 	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/kang/algorithms/a_sort/SortTestHelper.java
+++ src/main/java/com/kang/algorithms/a_sort/SortTestHelper.java
@@ -1,5 +1,6 @@
 package com.kang.algorithms.a_sort;
 
+import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.Random;
 
@@ -39,6 +40,42 @@ public class SortTestHelper {
 		for(int i = 0 ; i < a.length; i++) {
 			System.out.print(a[i] + " ");
 		}
+		System.out.println();
+	}
+	
+	/**
+	 * 对排序方法的性能测试
+	 * @param clazz
+	 * @param methodName
+	 * @param arr
+	 * @throws Exception
+	 */
+	public static void testSort(Class<?> clazz ,String methodName , int[] arr) throws Exception {
+		Object obj = clazz.newInstance();
+		Method method = clazz.getMethod(methodName, int[].class);
+		double startTime = new Date().getTime();
+		method.invoke(obj, arr);
+		double endTime = new Date().getTime();
+		System.out.println("是否排序成功：" + (isSorted(arr) ? "是" : "否"));
+		System.out.println("执行时间为：" + (endTime - startTime)/1000d + "s");
+	}
+
+	/**
+	 * 数组是否排序成功
+	 * @param arr
+	 */
+	public static boolean isSorted(int[] arr) {
+		for(int i = 0 ; i < arr.length - 1 ; i ++ ) {
+			if(arr[i] > arr[i+1])
+				return false;
+		}
+		return true;
+	}
+	
+	public static void swap(int[] arr,int i,int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 	
 }
