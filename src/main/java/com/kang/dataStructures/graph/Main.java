221ea5dc8bba9c71f16aa6607eeca57e176d3e73
kangruiwen <271858344@qq.com>
2018-03-20 14:10:41 +0800

    Graph Component

--- src/main/java/com/kang/dataStructures/graph/Main.java
+++ src/main/java/com/kang/dataStructures/graph/Main.java
@@ -37,5 +37,21 @@ public class Main {
         ReadGraph readGraph4 = new ReadGraph(g4, filename);
         System.out.println("test G2 in Dense Graph:");
         g4.show();
+        
+        
+     // TestG1.txt
+        /*String filename1 = "testG1.txt";
+        SparseGraph g1 = new SparseGraph(13, false);
+        ReadGraph readGraph1 = new ReadGraph(g1, filename1);
+        Components component1 = new Components(g1);
+        System.out.println("TestG1.txt, Component Count: " + component1.count());
+        System.out.println();*/
+
+        // TestG2.txt
+       /* String filename2 = "testG2.txt";
+        SparseGraph g2 = new SparseGraph(6, false);
+        ReadGraph readGraph2 = new ReadGraph(g2, filename2);
+        Components component2 = new Components(g2);
+        System.out.println("TestG2.txt, Component Count: " + component2.count());*/
     }
 }
